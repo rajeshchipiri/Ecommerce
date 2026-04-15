01ad44abfe8c1a0d25021c7a342e188d8b72c048
rajeshchipiri <rajeshchipiri5@gmail.com>
2026-04-15 19:59:48 +0530

    Fix: Externalize front-end URL in OAuth2 failure handler

--- backend/src/main/java/com/example/ecommerce/security/OAuth2AuthenticationFailureHandler.java
+++ backend/src/main/java/com/example/ecommerce/security/OAuth2AuthenticationFailureHandler.java
@@ -13,9 +13,12 @@ import java.io.IOException;
 @Component
 public class OAuth2AuthenticationFailureHandler extends SimpleUrlAuthenticationFailureHandler {
 
+    @org.springframework.beans.factory.annotation.Value("${app.frontend.url}")
+    private String frontendUrl;
+
     @Override
     public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
-        String targetUrl = UriComponentsBuilder.fromUriString("http://localhost:3000/login")
+        String targetUrl = UriComponentsBuilder.fromUriString(frontendUrl + "/login")
                 .queryParam("error", exception.getLocalizedMessage())
                 .build().toUriString();
 
